4fc6e3c1c44202557e25eb0bc7a6b1971194cb42
Mariya F <chibi.m@gmail.com>
2021-03-12 16:52:07 +0100

    After a lot of suffering with  sqlite3.OperationalError: unrecognized token: #, it hit me that I should probably delete all the # comments from my sql schema and replace them with slash-asterisk type. Wicked.

--- init_db.py
+++ init_db.py
@@ -1,26 +1,26 @@
 import sqlite3                                  # Importing sqlite3 module.
 
-connection = sqlite3.connect('database.db')     # Variable with connection to the database file `database.db` 
-                                                # which will be created as soon as python file is run.
+connection = sqlite3.connect('database.db')
+# Variable with connection to the database file `database.db` which will be created as soon as py file is run.
 # executescript() method creates a cursor object by calling the cursor() method, 
-# calls the cursor’s executescript() method with the given sql_script, 
-# and returns the cursor
+# calls the cursor’s executescript() method with the given sql_script, and returns the cursor
+
 with open('schema.sql') as f:
     connection.executescript(f.read())
 
-
-cur = connection.cursor()                       # Cursor object is created here but wtf is a cursor method
-                                                # Anyways, this cursor's execute() method will be called with the SQL script???
-                                                # After that, the cursor is returned?? what???? lmao
+cur = connection.cursor()
+# Cursor object is created in line 13 (cur=...) but wtf is a cursor method
+# Anyways, this cursor's execute() method will be called with the SQL script???
+# After that, the cursor is returned?? what???? lmao
 
 # Add data to DB:
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", 
-    ('First Post', 'Content for the first post')
-    )
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
+            ('First Post', 'Content for the first post')
+            )
 
 cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)",
-    ('Second Post', 'Content for the second post')
-    )
+            ('Second Post', 'Content for the second post')
+            )
 
 connection.commit()                             # Changes are committed here.
 connection.close()                              # And the connection is closed. Voila!
--- schema.sql
+++ schema.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS posts; # deletes all existing tables named `posts`, if any
+DROP TABLE IF EXISTS posts; /* deletes all existing tables named `posts`, if any*/
 
-CREATE TABLE posts (		# creates `posts` table
-    id INTEGER PRIMARY KEY AUTOINCREMENT,	# Filled automatically; primary key with unique value for every record
-    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,	# Filled automatically; NOT NULL = can't be empty; DEFAULT = time of post addition to base
-    title TEXT NOT NULL,	# Post title
-    content TEXT NOT NULL	# Post text
+CREATE TABLE posts (		/* creates `posts` table */
+    id INTEGER PRIMARY KEY AUTOINCREMENT,	/* Filled automatically; primary key with unique value for every record */
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,	/* Filled automatically; NOT NULL = can't be empty; DEFAULT = time of post addition to base*/
+    title TEXT NOT NULL,
+    content TEXT NOT NULL
 );
