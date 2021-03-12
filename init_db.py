48059e443564af12a35019743d9fcdb786bf8057
Mariya F <chibi.m@gmail.com>
2021-03-12 16:19:16 +0100

    lil fixes to init_db.py

--- init_db.py
+++ init_db.py
@@ -2,17 +2,19 @@ import sqlite3                                  # Importing sqlite3 module.
 
 connection = sqlite3.connect('database.db')     # Variable with connection to the database file `database.db` 
                                                 # which will be created as soon as python file is run.
+# executescript() method creates a cursor object by calling the cursor() method, 
+# calls the cursor’s executescript() method with the given sql_script, 
+# and returns the cursor
+with open('schema.sql') as f:
+    connection.executescript(f.read())
 
-with open('schema.sql') as f:                   
-    connection.executescript(f.read())          # executescript() method creates a cursor object by calling the cursor() method, 
-                                                # calls the cursor’s executescript() method with the given sql_script, 
-                                                # and returns the cursor
 
 cur = connection.cursor()                       # Cursor object is created here but wtf is a cursor method
                                                 # Anyways, this cursor's execute() method will be called with the SQL script???
                                                 # After that, the cursor is returned?? what???? lmao
 
-cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", # add data to DB
+# Add data to DB:
+cur.execute("INSERT INTO posts (title, content) VALUES (?, ?)", 
     ('First Post', 'Content for the first post')
     )
 
